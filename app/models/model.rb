b850833e8e39fd02d3b396854bbb4787f6aac24b
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-27 10:02:39 -0400

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,24 @@
 class Model
 
 end
+
+
+class Dog
+attr_reader :breed
+attr_accessor :age, :name
+@@all = []
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all <<self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+end
